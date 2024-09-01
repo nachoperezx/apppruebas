28bada71aa6e14c6c67fc2ef2c7b43773d005f21
ignacio perez <nachitoperez2015@gmail.com>
2024-09-01 02:19:03 -0400

    a

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Inicio from "./componentes/inicio";
+
+export default function App() {
+ 
+  return (
+    <View style={{flex:1}}>
+
+      <Inicio />
+    </View>
+  );
+}
+
+
---
+++ componentes/inicio.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+
+
+const Inicio = ( ) => {
+  
+ 
+ 
+
+  return (
+    <View style={{paddingTop:50,backgroundColor:"pink"}}>
+      <Text>hola</Text>
+    </View>
+  );
+};
+
+
+export default Inicio;
